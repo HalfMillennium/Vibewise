02003884db70683237017dd99f979ad7a2934065
Garrett Chestnut <gchestnu@stevens.edu>
2021-04-11 03:11:25 -0400

    Removed sensitive API keys for security.

--- core/public/retrieve-data.js
+++ core/public/retrieve-data.js
@@ -1,27 +1,6 @@
 function getData(id) {
     var token = localStorage.getItem("accessToken");
 
-    /*
-    ~TODO~
-      - download csv of values
-    
-    $.post({url: '/getdata', headers: {"Authorization": `Bearer ${token}`}}, { listID: "3Qm86XLflmIXVm1wcwkgDK"},
-         function(data) {
-        // "Data" is the array of track objects we get from the API. See server.js for the function that returns it.
-        console.log(data);
-
-        document.getElementById(id).style.visibility = "hidden";
-        
-        // example of csv creation with array values
-        const rows = [
-          ["name1", "city1", "some other info"],
-          ["name2", "city2", "more info"]
-        ];
-      
-        let csvContent = "data:text/csv;charset=utf-8," 
-            + rows.map(e => e.join(",")).join("\n");
-      });*/
-      
     $.ajaxSetup({
         headers: { "Authorization": `Bearer ${token}` }
     });
