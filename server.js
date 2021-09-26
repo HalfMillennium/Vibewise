7a6dff3b85374515556dd0f61d18663e03a789d5
Millennium <glc2266@gmail.com>
2021-09-26 19:47:17 -0400

    Optional port number from .env file (for Heroku)

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use(express.json());
 configRoutes(app)
 //-------------------------------------------------------------//
 
-app.listen('8888', function () {
+app.listen(process.env.PORT || '8888', function () {
   console.log(`Your app is listening on port ${routeNum}.`);
 });
