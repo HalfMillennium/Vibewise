eb4b59ab4a94a336906c8f5d1e12ade16e1c46a3
Millennium <glc2266@gmail.com>
2021-09-27 00:58:59 -0400

    Remove public from express static declaration

--- routes/client-routes.js
+++ routes/client-routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var path = require('path');
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 
