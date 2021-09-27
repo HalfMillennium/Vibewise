5d44002b7a882a708916798b3ad7f9dd6b0ed20d
Millennium <glc2266@gmail.com>
2021-09-27 14:23:05 -0400

    Modified headers for same-origin policy fix

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.set('view engine', 'handlebars');
 var uuid = require('uuid');
 //var exphbs = require('express-handlebars');
 //app.disable('etag');
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(session({secret: uuid.v4()}));
 app.use(express.json());
 
